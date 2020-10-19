62936600492f9e9a76e21ce97edc558f1eabe037
maiguiqi <962864309@qq.com>
2020-10-19 15:52:14 +0800

    2020.10.19

---
+++ src/main/java/com/leetcode/Jianzhi53_2.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+/**
+ * 剑指 Offer 53 - II. 0～n-1中缺失的数字
+ * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
+ * 在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [0,1,3]
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: [0,1,2,3,4,5,6,7,9]
+ * 输出: 8
+ *
+ * @author mai.gq
+ * @date 2020/10/19
+ */
+public class Jianzhi53_2 {
+    public int missingNumber(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (nums[m] == m) i = m + 1;
+            else j = m - 1;
+        }
+        return i;
+    }
+}
---
+++ src/main/java/com/leetcode/tree/Jianzhi55.java
@@ -0,0 +1,56 @@
+package com.leetcode.tree;
+
+import com.leetcode.TreeNode;
+
+/**
+ * 剑指 Offer 55 - II. 平衡二叉树
+ * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回 true 。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \
+ * 3   3
+ * / \
+ * 4   4
+ * 返回 false 。
+ *
+ * @author mai.gq
+ * @date 2020/10/19
+ */
+public class Jianzhi55 {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    private int height(TreeNode treeNode) {
+        if (treeNode == null) {
+            return 0;
+        }
+        int leftHeight = height(treeNode.left);
+        int rightHeight = height(treeNode.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+
+    }
+}
