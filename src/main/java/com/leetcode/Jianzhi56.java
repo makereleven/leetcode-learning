b0dfc376f5fdcb96db8296fc317166ad06094eba
maiguiqi <962864309@qq.com>
2020-10-29 14:30:35 +0800

    2020.10.29

---
+++ src/main/java/com/leetcode/Jianzhi56.java
@@ -0,0 +1,47 @@
+package com.leetcode;
+
+/**
+ * 剑指 Offer 56 - I. 数组中数字出现的次数
+ * 一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [4,1,4,6]
+ * 输出：[1,6] 或 [6,1]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,10,4,1,4,3,3]
+ * 输出：[2,10] 或 [10,2]
+ * <p>
+ * <p>
+ * 限制：
+ * <p>
+ * 2 <= nums.length <= 10000
+ *
+ * @author mai.gq
+ * @date 2020/10/22
+ */
+public class Jianzhi56 {
+    public int[] singleNumbers(int[] nums) {
+        int ret = 0;
+        for (int n : nums) {
+            ret ^= n;
+        }
+        int div = 1;
+        while ((div & ret) == 0) {
+            div <<= 1;
+        }
+        int a = 0, b = 0;
+        for (int n : nums) {
+            if ((div & n) != 0) {
+                a ^= n;
+            } else {
+                b ^= n;
+            }
+        }
+        return new int[]{a, b};
+
+    }
+}
