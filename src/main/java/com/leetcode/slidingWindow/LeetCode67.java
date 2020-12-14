e22124b5718031d95e9cd46b470ea4a1d931f265
maiguiqi <962864309@qq.com>
2020-12-14 14:19:21 +0800

    2020.12.14

---
+++ src/main/java/com/leetcode/slidingWindow/LeetCode67.java
@@ -0,0 +1,85 @@
+package com.leetcode.slidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 76. 最小覆盖子串
+ * 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+ * <p>
+ * 注意：如果 s 中存在这样的子串，我们保证它是唯一的答案。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "ADOBECODEBANC", t = "ABC"
+ * 输出："BANC"
+ * 示例 2：
+ * <p>
+ * 输入：s = "a", t = "a"
+ * 输出："a"
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length, t.length <= 105
+ * s 和 t 由英文字母组成
+ *
+ * @author mai.gq
+ * @date 2020/12/14
+ */
+public class LeetCode67 {
+    Map<Character, Integer> need = new HashMap<Character, Integer>();
+    Map<Character, Integer> window = new HashMap<Character, Integer>();
+
+    public String minWindow(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        int left = 0, right = 0;
+        // 记录最小覆盖子串的起始索引及长度
+        int start = 0, len = Integer.MAX_VALUE;
+        int sLen = s.length();
+        int valid = 0;
+        while (right < sLen) {
+            // c 是将移入窗口的字符
+            char c = s.charAt(right);
+            // 右移窗口
+            ++right;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+            /*** debug 输出的位置 ***/
+            System.out.println(String.format("window: [%d, %d)\n", left, right));
+
+            while (valid == need.size()) {
+                // 在这里更新最小覆盖子串
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                // d 是将移出窗口的字符
+                char d = s.charAt(left);
+
+                // 左移窗口
+                left++;
+                // 进行窗口内数据的一系列更新
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+        // 返回最小覆盖子串
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+
+}
