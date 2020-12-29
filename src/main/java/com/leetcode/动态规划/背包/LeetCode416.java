7fa042eaea158db4a11bfe5aa5cb6fa7da3287d0
maiguiqi <962864309@qq.com>
2020-12-29 18:22:16 +0800

    2020.12.29

---
+++ src/main/java/com/leetcode/动态规划/背包/LeetCode416.java
@@ -0,0 +1,66 @@
+package com.leetcode.动态规划.背包;
+
+/**
+ * 416. 分割等和子集
+ * 给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ * <p>
+ * 注意:
+ * <p>
+ * 每个数组中的元素不会超过 100
+ * 数组的大小不会超过 200
+ * 示例 1:
+ * <p>
+ * 输入: [1, 5, 11, 5]
+ * <p>
+ * 输出: true
+ * <p>
+ * 解释: 数组可以分割成 [1, 5, 5] 和 [11].
+ * <p>
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: [1, 2, 3, 5]
+ * <p>
+ * 输出: false
+ * <p>
+ * 解释: 数组不能分割成两个元素和相等的子集.
+ *
+ * @author mai.gq
+ * @date 2020/12/28
+ */
+public class LeetCode416 {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int num : nums) {
+            sum += num;
+            maxNum = Math.max(maxNum, num);
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        boolean[][] dp = new boolean[n][target + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 1; j <= target; j++) {
+                if (j >= num) {
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n - 1][target];
+    }
+}
---
+++ src/main/java/com/leetcode/动态规划/背包/LeetCode518.java
@@ -0,0 +1,51 @@
+package com.leetcode.动态规划.背包;
+
+/**
+ * 518. 零钱兑换 II
+ * 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: amount = 5, coins = [1, 2, 5]
+ * 输出: 4
+ * 解释: 有四种方式可以凑成总金额:
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ * 示例 2:
+ * <p>
+ * 输入: amount = 3, coins = [2]
+ * 输出: 0
+ * 解释: 只用面额2的硬币不能凑成总金额3。
+ * 示例 3:
+ * <p>
+ * 输入: amount = 10, coins = [10]
+ * 输出: 1
+ *
+ * @author mai.gq
+ * @date 2020/12/28
+ */
+public class LeetCode518 {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j - coins[i - 1] >= 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+}
