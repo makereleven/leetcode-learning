4e52b6e1f84c81860ba391dbabdc4598e8d8a15e
maiguiqi <962864309@qq.com>
2021-01-18 16:22:30 +0800

    2020.01.18

--- src/main/java/com/leetcode/动态规划/回文字符串/LeetCode5.java
+++ src/main/java/com/leetcode/动态规划/回文字符串/LeetCode5.java
@@ -1,7 +1,5 @@
 package com.leetcode.动态规划.回文字符串;
 
-import java.util.Arrays;
-
 /**
  * 5. 最长回文子串
  * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
@@ -21,50 +19,25 @@ import java.util.Arrays;
  */
 public class LeetCode5 {
     public String longestPalindrome(String s) {
-        int len = s.length();
-        if (len < 2) {
-            return s;
-        }
-
-        boolean[][] dp = new boolean[len][len];
-        for (int i = 0; i < len; i++) {
-            Arrays.fill(dp[i], false);
-        }
-
-        // 初始化
-        for (int i = 0; i < len; i++) {
-            dp[i][i] = true;
-        }
-
-
-        char[] charArray = s.toCharArray();
-        int maxLen = 1;
-        int start = 0;
-
-        for (int j = 1; j < len; j++) {
-            // 只有下面这一行和「参考代码 2」不同，i 正着写、倒过来写都行，因为子串都有参考值
-            for (int i = j - 1; i >= 0; i--) {
-
-                if (charArray[i] == charArray[j]) {
-                    if (j - i < 3) {
-                        dp[i][j] = true;
-                    } else {
-                        dp[i][j] = dp[i + 1][j - 1];
-                    }
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String ans = "";
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = true;
+                } else if (l == 1) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
                 } else {
-                    dp[i][j] = false;
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
                 }
-
-                // 只要 dp[i][j] == true 成立，就表示子串 s[i, j] 是回文，此时记录回文长度和起始位置
-                if (dp[i][j]) {
-                    int curLen = j - i + 1;
-                    if (curLen > maxLen) {
-                        maxLen = curLen;
-                        start = i;
-                    }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans = s.substring(i, i + l + 1);
                 }
             }
         }
-        return s.substring(start, start + maxLen);
+        return ans;
     }
+
 }
