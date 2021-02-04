8b98f539db7d0b6f7ae964e37ac0a4a6ce78bb94
maiguiqi <962864309@qq.com>
2021-02-04 14:09:44 +0800

    2020.02.04

---
+++ src/main/java/com/leetcode/DFS/LeetCode101.java
@@ -0,0 +1,46 @@
+package com.leetcode.DFS;
+
+import com.leetcode.TreeNode;
+
+/**
+ * 101. 对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * <p>
+ * <p>
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ * <p>
+ * <p>
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ *
+ * @author mai.gq
+ * @date 2021/2/4
+ */
+public class LeetCode101 {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+}
